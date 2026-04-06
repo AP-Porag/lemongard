8b6bb37974c136f92d7b1600428705cd5d15c810
unknown <razaulk127@gmail.com>
2026-04-06 19:09:08 +0600

    register, login, home page last updated 6/4

--- resources/js/components/public/Navigation.tsx
+++ resources/js/components/public/Navigation.tsx
@@ -98,7 +98,7 @@ const Navigation = () => {
             }`}
             aria-label="Main navigation"
         >
-            <div className="section-container">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     {/* Logo */}
                     <Link to="/" className="group flex items-center gap-2">
