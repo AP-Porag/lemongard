3ddda2445a479aec304db799847fa4770005fd49
AP-Porag <smauhp@gmail.com>
2026-04-10 12:35:02 +0600

    updated

--- resources/js/components/public/NavLink.tsx
+++ resources/js/components/public/NavLink.tsx
@@ -1,32 +1,28 @@
-import { Link, usePage } from '@inertiajs/react';
-import { forwardRef } from 'react';
-import { cn } from '@/lib/utils';
+import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
+import { forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
-interface NavLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-    href: string;
-    className?: string;
-    activeClassName?: string;
+interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+  className?: string;
+  activeClassName?: string;
+  pendingClassName?: string;
 }
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
-    ({ href, className, activeClassName, children, ...props }, ref) => {
-        const { url } = usePage();
-
-        const isActive = url === href || (href !== '/' && url.startsWith(href));
-
-        return (
-            <Link
-                ref={ref}
-                href={href}
-                className={cn(className, isActive && activeClassName)}
-                {...props}
-            >
-                {children}
-            </Link>
-        );
-    },
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
+  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+    return (
+      <RouterNavLink
+        ref={ref}
+        to={to}
+        className={({ isActive, isPending }) =>
+          cn(className, isActive && activeClassName, isPending && pendingClassName)
+        }
+        {...props}
+      />
+    );
+  },
 );
 
-NavLink.displayName = 'NavLink';
+NavLink.displayName = "NavLink";
 
 export { NavLink };
