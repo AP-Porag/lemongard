328468e2b3262326b2e3aa43126aaa2a8c37521e
Aongkon <jmaongkon733@gmail.com>
2026-05-03 11:37:23 +0600

    new commit

--- resources/js/pages/public/Contact.tsx
+++ resources/js/pages/public/Contact.tsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link } from "@inertiajs/react";
 import {
     ChevronRight,
     Mail,
