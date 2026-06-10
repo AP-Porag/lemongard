a79d225da923498b2ba058f9a421cf67ae30bc1a
unknown <razaulk127@gmail.com>
2026-06-10 11:55:06 +0600

    updated

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Services\Auth\GoogleAuthService;
+use Illuminate\Support\Facades\Auth;
 
 class GoogleAuthController extends Controller
 {
